db40252ef4321cf3c08d94b68b046611fcbcef05
Anna Sivongsak - notebook <sivonann@fit.cvut.cz>
2025-03-24 13:20:46 +0100

    Init 3

---
+++ src/main/java/cz/cvut/fel/pjv/dungeon_escape/Main.java
@@ -0,0 +1,15 @@
+package cz.cvut.fel.pjv.dungeon_escape;
+
+import cz.cvut.fel.pjv.dungeon_escape.view.GamePanel;
+import javafx.application.Application;
+
+public class Main {
+
+
+  public static void main(String[] args) {
+
+      System.out.println("Started game");
+      Application.launch(GamePanel.class, args);
+
+    }
+}
