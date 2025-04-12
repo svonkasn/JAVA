d294b42ca77ceddc2746fc9cb90ba23e6195f859
Anna Sivongsak - notebook <sivonann@fit.cvut.cz>
2025-04-12 19:26:58 +0200

    Added Enemy and GameState classes

---
+++ src/main/java/cz/cvut/fel/pjv/dungeon_escape/model/Enemy.java
@@ -0,0 +1,8 @@
+package cz.cvut.fel.pjv.dungeon_escape.model;
+
+public class Enemy extends GameItem {
+
+  public Enemy(ImageId imageId, double x, double y) {
+    super(imageId, x, y);
+  }
+}
---
+++ src/main/java/cz/cvut/fel/pjv/dungeon_escape/model/GameState.java
@@ -0,0 +1,4 @@
+package cz.cvut.fel.pjv.dungeon_escape.model;
+
+public class GameState {
+}
